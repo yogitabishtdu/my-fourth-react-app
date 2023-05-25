114ff55defc8ec975c59daca226b04daf2d45251
yogitabishtdu <yogitabishtdu@gmail.com>
2023-05-25 16:51:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <h2> My Fourth react App </h2>
+   <h3> I have made few changes today on 25 May</h3>
+   </>
   );
 }
 
